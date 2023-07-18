69f965e6c6493c3eff9f5b5db892db5045aad563
viet <viet1234561231@gmail.com>
2023-07-18 16:21:03 +0700

    khởi tạo

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View,Text } from 'react-native';
+
+const App =()=>{
+  return(
+    <View  className="flex justify-center">
+       <Text className="text-orange-700 ">HomeScreen</Text>
+    </View>
+  )
+}
+
+
+
+
+
+export default App;
